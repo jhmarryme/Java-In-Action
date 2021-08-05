1ad15edbd0bce8224568404498e584b14fbc517b
wjh <wjh@travelsky.com.cn>
2021-08-05 11:17:49 +0800

    update

--- mybatis-generator/src/main/java/com/jhmarryme/demo/mapper/system/log/CoderMapper.java
+++
@@ -1,7 +0,0 @@
-package com.jhmarryme.demo.mapper.system.log;
-
-import com.jhmarryme.demo.common.mybatis.BaseMapper;
-import com.jhmarryme.demo.pojo.model.system.log.Coder;
-
-public interface CoderMapper extends BaseMapper<Coder> {
-}
