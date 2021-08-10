f7bdd850ce697d41a5ac30ae5ae0e85f780dd954
wjh <wjh@travelsky.com.cn>
2021-08-10 20:29:40 +0800

    init spring-security-oauth2

---
+++ oauth/spring-5-security-oauth-jhmarryme/src/main/java/com/jhmarryme/Application.java
@@ -0,0 +1,18 @@
+package com.jhmarryme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author JiaHao Wang
+ * @date 2021/8/10 19:15
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ oauth/spring-5-security-oauth-jhmarryme/src/main/java/com/jhmarryme/controller/FooController.java
@@ -0,0 +1,18 @@
+package com.jhmarryme.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author JiaHao Wang
+ * @date 2021/8/10 19:14
+ */
+@RestController()
+public class FooController {
+
+    @GetMapping("/foo")
+    public String foo() {
+        return "success";
+    }
+}
