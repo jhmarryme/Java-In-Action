3442ade445946498f89c8ae52e5d56f75ec1a4b8
wjh <wjh@travelsky.com.cn>
2021-12-09 15:26:37 +0800

    feign 引用第三方接口时的配置

--- micro-service/spring-cloud-demo/feign/feign-consumer-advanced/src/main/java/com/example/springcloud/ConsumerAdvancedApplication.java
+++ micro-service/spring-cloud-demo/feign/feign-consumer-advanced/src/main/java/com/example/springcloud/ConsumerAdvancedApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignClient;
 
 /**
  *
@@ -15,6 +16,13 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableDiscoveryClient
 @EnableFeignClients
 public class ConsumerAdvancedApplication {
+
+    // 引用第三方接口时, 配置feign
+    // @FeignClient(value = "feign-client", primary = true)
+    // public interface MyService extends IService {
+    //
+    // }
+
 
     public static void main(String[] args) {
         new SpringApplicationBuilder(ConsumerAdvancedApplication.class)
