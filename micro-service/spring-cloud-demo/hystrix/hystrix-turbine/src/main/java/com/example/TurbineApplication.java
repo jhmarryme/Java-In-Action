f360f0acf27ca8759e652172b828fb1d85d87f3a
wjh <wjh@travelsky.com.cn>
2021-12-15 16:48:08 +0800

    turbine集成

---
+++ micro-service/spring-cloud-demo/hystrix/hystrix-turbine/src/main/java/com/example/TurbineApplication.java
@@ -0,0 +1,27 @@
+package com.example;
+
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+
+/**
+ *
+ * @author JiaHao Wang
+ * @date 2021/12/15 下午3:19
+ */
+@EnableDiscoveryClient
+@EnableTurbine
+@EnableHystrix
+@EnableAutoConfiguration
+public class TurbineApplication {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(TurbineApplication.class)
+                .web(WebApplicationType.SERVLET)
+                .run(args);
+    }
+}
