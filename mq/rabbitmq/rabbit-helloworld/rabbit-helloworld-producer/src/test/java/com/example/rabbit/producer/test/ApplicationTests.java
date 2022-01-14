c5e8ba9ec1b75a1e5bc4c5ce7fe35beeff3794dd
wjh <wjh@travelsky.com.cn>
2022-01-14 16:55:32 +0800

    update rabbit

---
+++ mq/rabbitmq/rabbit-helloworld/rabbit-helloworld-producer/src/test/java/com/example/rabbit/producer/test/ApplicationTests.java
@@ -0,0 +1,46 @@
+package com.example.rabbit.producer.test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.example.rabbit.producer.component.RabbitSender;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApplicationTests {
+
+	
+	@Autowired
+	private RabbitSender reRabbitSender;
+	
+	
+	@Test
+	public void testSender() throws Exception {
+		Map<String, Object> properties = new HashMap<String, Object>();
+		properties.put("attr1", "12345");
+		properties.put("attr2", "abcde");
+		reRabbitSender.send("hello rabbitmq!", properties);
+		
+		Thread.sleep(10000);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
